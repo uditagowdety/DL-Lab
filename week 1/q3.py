3939361f5036e5072c796b663c869cc747373c2f
udita gowdety <uditagowdety@gmail.com>
2025-01-06 16:15:35 +0530

    annotated q3

--- week 1/q3.py
+++ week 1/q3.py
@@ -1,8 +1,14 @@
 import torch
 
-tensor=torch.rand(9)
+# create a 1D tensor with random values between 0 and 1, of size 9
+tensor = torch.rand(9)
 print(tensor)
 
+# accessing the element at index 4 (5th element) of the tensor
 print(tensor[4])
+
+# accessing the last element of the tensor using negative indexing
 print(tensor[-1])
-print(tensor[[1,3,4]])
+
+# accessing multiple elements at indices 1, 3, and 4 using a list of indices
+print(tensor[[1, 3, 4]])
