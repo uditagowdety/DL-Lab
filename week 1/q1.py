e9a2a8e674a9b20dcc33ad15f683006e90dab8c4
udita gowdety <uditagowdety@gmail.com>
2025-01-06 16:13:45 +0530

    annotated q1

--- week 1/q1.py
+++ week 1/q1.py
@@ -1,34 +1,35 @@
 import torch
 
-tensor=torch.arange(9)
+# create a 1D tensor with values from 0 to 8
+tensor = torch.arange(9)
 print(tensor)
 print()
 
-#reshaping a tensor:
-reshaped_tensor=torch.reshape(tensor,(3,3))
+# reshaping a tensor to a 3x3 matrix
+reshaped_tensor = torch.reshape(tensor, (3, 3))
 print(reshaped_tensor)
 print()
 
-#viewing a tensor:
-view_tensor=tensor.view(3,3)
+# viewing a tensor (creating a new view of the same data without copying it)
+view_tensor = tensor.view(3, 3)
 print(view_tensor)
 print()
 
-#stacking 2 tensors:
-tensor_1=torch.tensor([1,2])
-tensor_2=torch.tensor([3,4])
-stacked_tensor=torch.stack([tensor_1,tensor_2],dim=0)
+# stacking 2 tensors along a new dimension (dim=0, creating a 2x2 tensor)
+tensor_1 = torch.tensor([1, 2])
+tensor_2 = torch.tensor([3, 4])
+stacked_tensor = torch.stack([tensor_1, tensor_2], dim=0)
 print(stacked_tensor)
 print()
 
-#squeezing a tensor:
-new_tensor=torch.zeros(1,2,3)
+# squeezing a tensor (removes dimensions with size 1)
+new_tensor = torch.zeros(1, 2, 3)  # tensor with shape (1, 2, 3)
 print(new_tensor)
 print()
-squeeze_tensor=torch.squeeze(new_tensor,dim=0)
+squeeze_tensor = torch.squeeze(new_tensor, dim=0)  # removes the dimension with size 1 at index 0
 print(squeeze_tensor)
 print()
 
-#unsqueezing a tensor
-unsqueeze_tensor=torch.unsqueeze(tensor_1,dim=0)
+# unsqueezing a tensor (adds a new dimension with size 1 at the specified index)
+unsqueeze_tensor = torch.unsqueeze(tensor_1, dim=0)  # adds a new dimension at index 0
 print(unsqueeze_tensor)
