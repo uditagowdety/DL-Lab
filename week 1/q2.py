ec1eefa527abbec631f9ced1240c7c0feafed01f
udita gowdety <uditagowdety@gmail.com>
2025-01-06 16:15:07 +0530

    annotated q2

--- week 1/q2.py
+++ week 1/q2.py
@@ -1,7 +1,10 @@
 import torch
 
-original=torch.randn(1,3,5)
+# create a tensor with random values of shape (1, 3, 5)
+original = torch.randn(1, 3, 5)
 print(f"original tensor dimensions:  {original.shape}")
 
-permuted=original.permute(1,0,2)
+# permuting the dimensions of the tensor. This swaps axes 0 and 1.
+# the new shape becomes (3, 1, 5)
+permuted = original.permute(1, 0, 2)
 print(f"permuted tensor dimensions:  {permuted.shape}")
