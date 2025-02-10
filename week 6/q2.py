c643949b87f10f979083e14908ae52a83c95aad5
udita gowdety <uditagowdety@gmail.com>
2025-02-10 16:02:51 +0530

    start alexnet code

--- week 6/q2.py
+++ week 6/q2.py
@@ -1,1 +1,16 @@
-#alex net cats and dogs
+#alexnet cats and dogs
+
+import PIL.Image
+import torch
+from torch.utils.data import Dataset, DataLoader
+import torch.nn as nn
+from torchvision import transforms
+import glob
+from torchvision.models import AlexNet_Weights
+
+model=torch.hub.load("pytorch/vision:v0.10.0",model="alexnet",weights=AlexNet_Weights.DEFAULT)
+batch_size=4
+
+criterion=nn.CrossEntropyLoss
+optimizer=torch.optim.SGD(model.parameters(),lr=0.001)
+
